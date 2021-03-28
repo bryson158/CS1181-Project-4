03732c05ee5be1e98b2537ca17fb3492fc93488f
bryson <bryson158>
2021-03-27 22:40:10 -0400

    test

--- java/Game.java
+++ java/Game.java
@@ -5,24 +5,30 @@ import java.util.Scanner;
 public class Game {
     //Todo- Determine if the rng is needed in this class
     //Random rng = new Random();
-    ArrayList<Player> playerList = new ArrayList<>();
-    Human humanPlayer = new Human();
-    Deck deckOfCards = new Deck();
 
     public static void main(String[] args){
-
+        game();
     }
 
     //Main method for the game
     public static void game(){
         Scanner input = new Scanner(System.in);
+        Deck deckOfCards = new Deck();
+        Human humanPlayer = new Human();
 
+        ArrayList<Player> playersList = new ArrayList<>();
 
+        playersList.add(humanPlayer);
 
-        for(int i = 0; i < 4; i++){
-
+        for(int i = 0; i < 3; i++){
+            playersList.add(new Robot());
         }
 
+        //Calls the method to deal the first cards
+        dealFristCards(playersList, deckOfCards);
+
+
+        
     }
 
     //Checks if the player has won the game
@@ -30,11 +36,16 @@ public class Game {
         return false;
     }
 
-    private void dealFristCards(ArrayList<Player> players){
+    //Deals out the first round of cards
+    private static void dealFristCards(ArrayList<Player> players, Deck deck){
         for(int i = 0; i < players.size(); i++){
             while (players.get(i).handSize() < 4){
-                players.get(i).addCardToHand(deckOfCards.dealCard());
+                players.get(i).addCardToHand(deck.dealCard());
             }
         }
+    }
+
+    private static String getCardString(){
+
     }
 }
